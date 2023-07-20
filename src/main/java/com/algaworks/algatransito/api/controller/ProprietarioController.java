c25fede4def46254d3b97f4ae5e4cb1e9a1c7525
Giovanne Mendonca <giovannemendonca@outlook.com>
2023-07-20 17:18:57 -0300

    messages de propiedades de error

--- src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -64,10 +64,4 @@ public class ProprietarioController {
     registroProprietarioService.excluir(proprietarioId);
     return ResponseEntity.noContent().build();
   }
-
-  @ExceptionHandler(NegocioException.class)
-  public ResponseEntity<String> capturar( NegocioException e ){
-    return ResponseEntity.badRequest().body(e.getMessage());
-  }
-
 }
